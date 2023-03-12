a270720496a7118ea09ed1fe3155ddf2e8112997
Mohamed Fayis <fayis@outlook.in>
2023-03-13 00:52:56 +0530

    key hard coded

--- src/lib/ghost.ts
+++ src/lib/ghost.ts
@@ -3,6 +3,6 @@ import GhostContentAPI from '@tryghost/content-api';
 // Create API instance with site credentials
 export const ghostClient = new GhostContentAPI({
     url: 'https://fayis.digitalpress.blog', // This is the default URL if your site is running on a local environment
-    key: import.meta.env.CONTENT_API_KEY,
+    key: 'b689e80c17552ce7847939b838',
     version: 'v5.0',
 });
