6b95135e850badcf8d48e28de69ca7b4630a0268
Mohamed Fayis <fayis@outlook.in>
2023-03-13 14:06:03 +0530

    testing out ga with party

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,18 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 // import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-// import partytown from "@astrojs/partytown";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fayis.in',
   integrations: [
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
     tailwind(),
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
